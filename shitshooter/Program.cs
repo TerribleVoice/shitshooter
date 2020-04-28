e7abe578b8fcff048a1e8ae70f66e29a043b36a1
terriblevoice <terriblevoice@ya.ru>
2020-04-28 20:51:01 +0500

    init project

---
+++ shitshooter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace shitshooter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
