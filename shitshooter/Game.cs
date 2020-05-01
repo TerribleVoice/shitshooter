78449a37389f8ad0f530df980e2afcffe26eba37
terriblevoice <terriblevoice@ya.ru>
2020-05-01 20:01:57 +0500

    Created map in Game and edit StartGame
    Now StartGame is also fills map

--- shitshooter/Game.cs
+++ shitshooter/Game.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 
 namespace ShitShooter
 {
@@ -8,19 +10,31 @@ namespace ShitShooter
         public Player Player { get; private set; }
         public List<Target> Targets { get; private set; }
         public List<Bullet> Bullets { get; }
+        public ICreature[,] Map;
 
-        public int Width { get; }
-        public int Height { get; }
+
+        public int Width => Map.GetLength(0);
+        public int Height => Map.GetLength(1);
 
         public Game(int width, int height, List<Bullet> bullets)
         {
-            Width = width;
-            Height = height;
+            Map = new ICreature[width, height];
             Bullets = bullets;
         }
 
         public void StartGame(Player player, List<Target> targetsList)
         {
+            if(!PointBelongsMap(player.Position))
+                throw new ArgumentException("Player is outside the map");
+            Map[player.Position.X, player.Position.Y] = player;
+
+            foreach (var target in targetsList)
+            {
+                if(!PointBelongsMap(target.Position))
+                    throw new ArgumentException($"target ({target.Position.X},{target.Position.Y}) is outside the map");
+                Map[target.Position.X, target.Position.Y] = target;
+            }
+
             Player = player;
             Targets = targetsList;
         }
@@ -42,6 +56,11 @@ namespace ShitShooter
             target.Hp -= bullet.Damage;
             if (target.Hp <= 0)
                 Targets.Remove(target);
+        }
+
+        private bool PointBelongsMap(Point point)
+        {
+            return point.X >= 0 && point.Y >= 0 && point.X < Width && point.Y < Height;
         }
     }
 }
