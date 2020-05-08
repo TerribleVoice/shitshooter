77a4d0a8289d22b71601fc83e490139fe9d0e059
terriblevoice <terriblevoice@ya.ru>
2020-05-08 23:54:07 +0500

    created start file

--- shitshooter/Program.cs
+++ shitshooter/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ShitShooter
 {
     class Program
     {
+        public static List<Target> defaultTargets = new List<Target>
+        {
+            new Target(new Point(1, 0), 1),
+            new Target(new Point(2, 0), 1),
+            new Target(new Point(3, 0), 1)
+        };
+
         static void Main(string[] args)
         {
+            var Game = new Game(5,5);
+            var Player = new Player(new Point(2,4), Game);
+            var targets = defaultTargets;
+            Application.Run(new Form1(Game, Player, defaultTargets));
         }
     }
 }
